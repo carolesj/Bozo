26a98b46495c7ab690270b12df5f7ab54f5a5e17
carolesj <caroline.jesuino.silva@usp.br>
2017-03-29 22:11:04 -0300

    Problemas menores resolvidos

--- Dado.java
+++ Dado.java
@@ -1,74 +1,85 @@
 /**
  * Created by caroles on 28/03/2017.
  */
+
+/**Representante de um dado de n lados.
+ * @author Caroline Jesuíno (9293925); Beatriz Monteiro(9778619)
+ */
+
+
 public class Dado extends java.lang.Object {
-    public Dado (int lados)  {
+    /**Construtor que recebe o número de lado dos dados
+     * @param lados número de faces dos dados
+     * */
+    public Dado(int lados) {
         this.lados = lados;
     }
-    public Dado () {
+
+    /**Construtor default que seta o número de faces como 6*/
+    public Dado() {
         this.lados = 6;
     }
+
     int lados;
     public int ultimoLancamento;
 
-    Random resultado = new Random();
+    private static Random resultado = new Random();
 
-    public int rolar () {
-        ultimoLancamento = (Random.getIntRand(this.lados) + 1);
+    /**Rola o dado, usando um rand até o valor mais alto (número de faces).
+     * @return o valor do dado lançado*/
+    public int rolar() {
+        ultimoLancamento = (resultado.getIntRand(this.lados) + 1);
         return this.ultimoLancamento;
     }
 
-    public int getLado () {
+    /**Retorna o valor da face virada para cima da última jogada
+     * @return o valor do dado lançado na última rodada*/
+    public int getLado() {
         return ultimoLancamento;
     }
 
+    /**Sobrescreve o método toString() da java.lang.Object, alterando sua função para representar um dado
+     * em estilo ASCII art.*/
     @Override
-    public String toString (int face) {
-
-        switch (face) {
+    public String toString() {
+        switch (ultimoLancamento) {
             case 1:
                 return "+-----+" +
                         "|     |" +
                         "|  *  |" +
                         "|     |" +
                         "+-----+";
-                break;
             case 2:
                 return "+-----+" +
                         "|*    |" +
                         "|     |" +
                         "|    *|" +
                         "+-----+";
-                break;
             case 3:
                 return "+-----+" +
                         "|*    |" +
                         "|  *  |" +
                         "|    *|" +
                         "+-----+";
-                break;
             case 4:
                 return "+-----+" +
                         "|*   *|" +
                         "|     |" +
                         "|*   *|" +
                         "+-----+";
-                break;
             case 5:
                 return "+-----+" +
                         "|*   *|" +
                         "|  *  |" +
                         "|*   *|" +
                         "+-----+";
-                break;
             case 6:
                 return "+-----+" +
                         "|*   *|" +
                         "|*   *|" +
                         "|*   *|" +
                         "+-----+";
-                break;
         }
+        return null;
     }
-
 }
--- RolaDados.java
+++ RolaDados.java
@@ -2,65 +2,115 @@
  * Created by caroles on 28/03/2017.
  */
 
+/**Auxilia o gerenciamento de múltiplos dados
+ * @author Caroline Jesuíno (9293925); Beatriz Monteiro(9778619)
+ */
+
+
 import java.lang.Integer;
 import java.lang.String;
 
+/**Contrutor que seta o número de dados, cria um vetor de dados e o aloca
+ * @param n número de dados em jogo
+ * */
 public class RolaDados {
     public RolaDados (int n) {
         this.numeroDeDados = n;
         listaDeDados = new Dado[n];
+        for (int i = 0; i < n; i++) {
+            listaDeDados[i] = new Dado();
+        }
     }
     Dado listaDeDados[];
     int numeroDeDados;
 
+    /**Rola todos os dados usando uma classe que gera números aleatórios
+     * @return vetor de inteiros com os valores das faces para cima dos dados*/
     public int[] rolar () {
         int resultado[];
         resultado = new int[numeroDeDados];
         for (int i = 0; i < numeroDeDados; i++) {
-            resultado[i] = Dado.rolar(listaDeDados[i]);
+            resultado[i] = listaDeDados[i].rolar();
         }
         return resultado;
     }
 
+    /**Rola alguns dados usando uma classe que gera números aleatórios
+     * @param quais vetor de booleanos que indica quais dados devem ser rolados
+     * @return vetor de inteiros com o valor das faces para cima
+     * */
     public int [] rolar (boolean[] quais) {
         int resultado[];
         resultado = new int[numeroDeDados];
         for (int i = 0; i < numeroDeDados; i++) {
             if (quais[i]) {
-                resultado[i] = Dado[i].rolar();
+                resultado[i] = listaDeDados[i].rolar();
             } else {
-                resultado[i] = Dado[i].getLados();
+                resultado[i] = listaDeDados[i].getLado();
             }
         }
         return resultado;
     }
 
+    /**Rola alguns dados usando uma classe que gera números aleatórios
+     * @param s string que indica quais dados devem ser rolados
+     * @return vetor de inteiros com o valor das faces para cima
+     * */
     public int [] rolar (String s) {
-        int resultado[];
+        int resultado[], auxiliar[];
         resultado = new int[numeroDeDados];
         /*O vetor separados receberá cada substring da nossa string original,
          * as quais transformaremos em int*/
         String separados[];
-        int tamVet = s.length() / 2;
-        separados = new String[tamVet];
         separados = s.split(" ");
-        for (int i = 0; i < separados.length(); i++) {
-            resultado[i] = Integer.parseInt(separados[i]);
+        int tam = separados.length;
+        auxiliar = new int[tam];
+        for (int i = 0; i < tam; i++) {
+            auxiliar[i] = Integer.parseInt(separados[i]) - 1;
         }
+
         /*Acessa as posições no vetor que guardam quais dados devem ser rolados. Rola
         * esses dados.*/
-        for (int i = 0; i < separados.length(); i++) {
-            listaDeDados[resultado[i]] = Dado[resultado[i]].rolar();
+        for (int i = 0; i < tam; i++) {
+            listaDeDados[auxiliar[i]].rolar();
+        }
+        /*Atribui todos os valores (rolados e não rolados) para o vetor de resultados*/
+        for (int i = 0; i < numeroDeDados; i++) {
+            resultado[i] = listaDeDados[i].getLado();
         }
         return resultado;
     }
 
+    /**Sobrescreve o método toString() da java.lang.Object, alterando sua função para representar uma sequência de
+     *dados horizontalmente em estilo ASCII art.
+     * @return a string que contém a representação
+     * */
     @Override
     public String toString () {
-        String dadosResultantes;
-        for (int i = 0; i < 5; i++) {
-            dadosResultantes += Dado.toString(listaDeDados[i]);
+
+        String result = " 1        2        3        4        5\n+-----+  +-----+  +-----+  +-----+  +-----+\n";
+        for(int i=0; i< 5; i++) {
+            if (listaDeDados[i].getLado() == 1) result +="|     |  ";
+            else if (listaDeDados[i].getLado() == 2 || listaDeDados[i].getLado() == 3) result+="|*    |  ";
+            else result+="|*   *|  ";
+            if (i == 4) result+="\n";
         }
-        return dadosResultantes;
+
+        for(int i=0; i< 5; i++) {
+            if (listaDeDados[i].getLado() == 1 || listaDeDados[i].getLado() == 3 || listaDeDados[i].getLado() == 5) result+="|  *  |  ";
+            else if (listaDeDados[i].getLado() == 6) result+="|*   *|  ";
+            else result+="|     |  ";
+            if (i == 4) result+="\n";
+        }
+
+        for(int i=0; i< 5; i++) {
+            if (listaDeDados[i].getLado() == 2 || listaDeDados[i].getLado() == 3) result+="|    *|  ";
+            else if (listaDeDados[i].getLado() == 1) result+="|     |  ";
+            else result+="|*   *|  ";
+            if (i == 4) result+="\n";
+        }
+
+        result+="+-----+  +-----+  +-----+  +-----+  +-----+\n";
+        return result;
     }
 }
